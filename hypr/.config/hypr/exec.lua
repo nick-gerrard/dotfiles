3f99dd7caec0398c50fe8d71f21a24494cd9919a
Nick Gerrard <nick.gerrard16@gmail.com>
2026-05-21 10:58:15 -0400

    fixed hyprpaper

--- hypr/.config/hypr/exec.lua
+++ hypr/.config/hypr/exec.lua
@@ -1,10 +1,6 @@
--- exec-once = waybar & hyprpaper & hypridle
--- exec-once = kitty
--- # exec-once = sh -c 'XAPP_FORCE_GTKWINDOW_ICON="call-start" firefox --class WebApp-WhatsApp6244 --name WebApp-WhatsApp6244 --profile /home/nickg/.local/share/ice/firefox/WhatsApp6244 --no-remote "https://web.whatsapp.com"'
-
 hl.on("hyprland.start", function()
 	hl.exec_cmd("waybar")
 	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("hypridle")
-	hl.exec_cmd("kitty")
+	hl.exec_cmd("ghostty")
 end)
